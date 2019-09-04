9525f1cfa884a2c5d711c8c5cacaf0e5c97762b5
Gabe Castro <gcastro103@gmail.com>
2019-09-04 13:22:14 -0500

    data and message classes

--- app/src/main/java/com/github/maxmobility/wearmessage/Data.java
+++ app/src/main/java/com/github/maxmobility/wearmessage/Data.java
@@ -1,4 +1,50 @@
 package com.github.maxmobility.wearmessage;
 
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.Date;
+
 public class Data {
+
+    private static final String TAG = "Data";
+
+    private static final String DATA_PATH = "/user-data";
+    private static final String DATA_KEY = "data";
+    private Context mContext;
+
+    public Data(Context ctx) {
+        mContext = ctx;
+    }
+    private static Asset toAsset(byte[] item){
+        Asset returnAsset = Asset.createFromBytes(item);
+        return returnAsset;
+    }
+    public void sendData(String data) {
+        Log.d(TAG, "Data to be sent: " + data);
+        PutDataMapRequest dataMap = PutDataMapRequest.create(DATA_PATH);
+        dataMap.getDataMap().putString(DATA_KEY, data);
+        dataMap.getDataMap().putLong("time", new Date().getTime());
+        PutDataRequest request = dataMap.asPutDataRequest();
+        request.setUrgent();
+
+        Task<com.google.android.gms.wearable.DataItem> dataItemTask = Wearable.getDataClient(mContext).putDataItem(request);
+
+        dataItemTask.addOnSuccessListener(
+                new OnSuccessListener<com.google.android.gms.wearable.DataItem>() {
+                    @Override
+                    public void onSuccess(com.google.android.gms.wearable.DataItem dataItem) {
+                        Log.d(TAG, "Sending data was successful: " + dataItem);
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/github/maxmobility/wearmessage/Message.java
+++ app/src/main/java/com/github/maxmobility/wearmessage/Message.java
@@ -1,4 +1,99 @@
 package com.github.maxmobility.wearmessage;
 
+
+
+import android.content.Context;
+
+import android.os.AsyncTask;
+import androidx.annotation.WorkerThread;
+import android.util.Log;
+
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.Node;
+
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class Message {
+    private static final String TAG = "Message";
+
+    private String mPath = "/start-message";
+    private Context mContext;
+
+    private String mMessage;
+
+    public Message(Context ctx) {
+        mContext = ctx;
+    }
+
+    public void sendMessage(String messagePath, String message){
+        mPath = messagePath;
+        mMessage = message;
+        new StartWearableTask().execute();
+    }
+
+    @WorkerThread
+    private Collection<String> getNodes() {
+        HashSet<String> results = new HashSet<>();
+
+        Task<List<Node>> nodeListTask =
+                Wearable.getNodeClient(mContext.getApplicationContext()).getConnectedNodes();
+
+        try {
+            // Block on a task and get the result synchronously (because this is on a background
+            // thread).
+            List<Node> nodes = Tasks.await(nodeListTask);
+
+            for (Node node : nodes) {
+                results.add(node.getId());
+            }
+
+        } catch (ExecutionException exception) {
+            Log.e(TAG, "Task failed: " + exception);
+
+        } catch (InterruptedException exception) {
+            Log.e(TAG, "Interrupt occurred: " + exception);
+        }
+
+        return results;
+    }
+
+    @WorkerThread
+    private void sendStartMessage(String node) {
+
+        Task<Integer> sendMessageTask =
+                Wearable.getMessageClient(mContext).sendMessage(node, mPath, mMessage.getBytes() );
+
+        try {
+            // Block on a task and get the result synchronously (because this is on a background
+            // thread).
+            Integer result = Tasks.await(sendMessageTask);
+            Log.d(TAG, "Message sent: " + result);
+
+        } catch (ExecutionException exception) {
+            Log.e(TAG, "Task failed: " + exception);
+
+        } catch (InterruptedException exception) {
+            Log.e(TAG, "Interrupt occurred: " + exception);
+        }
+    }
+
+    private class StartWearableTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... args) {
+            Collection<String> nodes = getNodes();
+            for (String node : nodes) {
+                sendStartMessage(node);
+            }
+            return null;
+        }
+    }
+
 }
