14648c7d68ce1cbc616c7ee6f260d3e18d859eef
Brad Martin <bradwaynemartin@gmail.com>
2019-09-26 12:17:51 -0500

    remove label from application node in lib manifest

--- app/src/main/java/com/github/maxmobility/wearmessagetest/MainActivity.java
+++ app/src/main/java/com/github/maxmobility/wearmessagetest/MainActivity.java
@@ -1,8 +1,9 @@
 package com.github.maxmobility.wearmessagetest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MAIN", com.github.maxmobility.wearmessage.Data.class.toString());
     }
 }
